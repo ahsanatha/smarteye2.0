771c5737102c2d3e040657b2a356fa65c5739701
ahsanatha <ahsan.athallah@gmail.com>
2019-11-11 12:56:07 +0700

    update

--- server.py
+++ server.py
@@ -1,26 +1,64 @@
 from flask import Flask, escape, request, render_template
 import glob
+import json
+from utils.bikinvideo import generateSuspectVideo
+from utils.find_suspect import find_suspect, find_idx_suspect
+import os
+import shutil
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET', 'POST'])
-def root():
+@app.route('/dashboard', methods=['POST'])
+def dashboard():
+    if (request.form.get('Username') == 'admin' and request.form.get('admin')):
+        return render_template('dashboard.html')
+    else:
+        return home()
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/test',methods=['GET', 'POST'])
+def test():
     wildCamera = glob.glob('static\\Camera *')
     cameras = []
     for i in wildCamera:
         filename = i.split("\\")[1]
         id = filename.split()[1]
-        # filename = filename.split()[0]+'_'+filename.split()[1]
         cameras.append(filename)
     if request.method == 'GET':
         camera = "Camera 0"
     elif request.method == "POST":
         camera = request.form.get("Camera")
-        
     return render_template('index.html', cameras = cameras, camera=camera)
 
-# @app.route('/takeframe')
-# def getFrame()
-
+@app.route('/findSuspect', methods=["POST"])
+def getFrame():
+    cam = request.form.get('camera')
+    frameId = request.form.get('frameId')
+    cam = cam.split()[1]
+    with open('static\\Json '+cam+'\\0.json') as json_file:
+        data = json.load(json_file)
+    selectedData = data['data'][int(frameId)]['object']
+    suspect_ft = selectedData[0]['feature']
+    target_dir = 'static/res/'
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    if len(suspect_ft) > 0 :
+        frame_num, suspect_idx = find_idx_suspect(cam, suspect_ft, target_dir + str(cam))
+        if (frame_num is not -1):
+            find_suspect(cam, frame_num, suspect_ft, target_dir + str(cam), 'forward')
+            find_suspect(cam, frame_num, suspect_ft, target_dir + str(cam), 'backward')
+        frames = set([int(i[0]) for i in [i[3].split('.') for i in [i.split('\\') for i in glob.glob('static\\res\\'+str(cam)+'\\*.jpg')]]])
+        first_frame = min(frames)
+        # last_frame = len(frames)
+        print(first_frame)
+        path = generateSuspectVideo('static\\res\\'+str(cam), first_frame)
+    else :
+        path = 'None'
+    return path
 if __name__ == '__main__':
-   app.run(host="0.0.0.0" ,debug = True)
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(host="0.0.0.0" ,debug = True)
--- utils/bikinvideo.py
+++ utils/bikinvideo.py
@@ -3,7 +3,7 @@ import os
 
 pathVid = []
 def generateAllVideo():
-    global videos
+    global pathVid
     wildCamera = glob.glob('..\\static\\Camera *')
     for i in wildCamera:
         filename = i.split("\\")[2]
@@ -14,11 +14,18 @@ def generateAllVideo():
         os.system('ffmpeg -framerate 10 -i "'+i+'"/'+'%d.jpg ../static/vid/"'+filename+'".mp4')
         os.system('ffmpeg -i ../static/vid/"'+filename+'".mp4 -codec copy -f dash -window_size 10 -min_seg_duration 30 -use_timeline 1 -init_seg_name '+str(id)+'-init.m4s -media_seg_name '+str(id)+'-$Time$.m4s ../static/vid/"'+filename+'".mpd')
 
+def generateSuspectVideo(filepath, start):
+    filename = 'suspect'+filepath.split('\\')[2]
+    id = filename
+    os.system('ffmpeg -framerate 10 -i "'+filepath+'"\\'+'%d.jpg  static/vid/"'+filename+'".mp4')
+    os.system('ffmpeg -i static/vid/"'+filename+'".mp4 -codec copy -f dash -min_seg_duration 30 -init_seg_name '+str(id)+'-init.m4s -media_seg_name '+str(id)+'-$Time$.m4s static/vid/"'+filename+'".mpd')
+    return filename+'.mp4'
+    
 def convertToMPD(pathVid):
     for i in pathVid:
         os.system('ffmpeg -i '+i+'.mp4 -c copy -f dash ../static/vid/Camera_0.mpd')
 def apahayo():
-    print("apaaa")
+    return "apaaa"
 
 if __name__ == "__main__":
     generateAllVideo()
